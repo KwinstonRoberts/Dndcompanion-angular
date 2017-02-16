db2f40140c85e9ae5c850078e6c733ba1d668c71
KyleW <kyle.w.r@hotmail.com>
2017-02-15 21:45:53 -0500

    added languages

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,7 +7,7 @@ const API = 'mongodb://KyleR:351797asd@ds151289.mlab.com:51289/heroku_6v949zvj';
 mongoose.connect(API);
 
 
-router.get('/api/find',function(req,res) {
+router.get('/api',function(req,res) {
 
   User.find({},function(err, user) {
     if (err) console.log(err);
