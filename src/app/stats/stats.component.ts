c44c3fef0aaa51ef6a3771b0cdeb27c44bac8fb5
KyleW <kyle.w.r@hotmail.com>
2017-02-15 19:34:03 -0500

    fixed stats

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -9,5 +9,5 @@ import {ModifierService} from "../modifier.service";
 export class StatsComponent {
 
   constructor(public modifier:ModifierService){}
-  public stats:any[] = [["Strength",0],["Dexterity",0],["Constitution",0],["Wisdom",0], ["Intelligence",0],["Charisma",0]];
+  public stats:any[] = [["Strength",0],["Dexterity",0],["Constitution",0],["Intelligence",0], ["Wisdom",0],["Charisma",0]];
 }
