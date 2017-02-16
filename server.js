e99c8356f8ae7c22cc7503c27e743085b5fa5bb5
KyleW <kyle.w.r@hotmail.com>
2017-02-16 15:07:38 -0500

    fixed build

--- server.js
+++ server.js
@@ -1,58 +1,115 @@
-// Get dependencies
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
-const axios = require('axios');
+var USER_COLLECTION = "users";
 
-const API = 'mongodb://KyleR:351797asd@ds151289.mlab.com:51289/heroku_6v949zvj';
-
-const app = express();
-
-
-
-// Parsers for POST data
+var app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
 
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-// Catch all other routes and return the index file
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+// CONTACTS API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+app.get("*",function(req,res){
+  res.sendFile(__dirname, '/dist/index.html');
 });
 
 
+/*  "/api/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
 
-// Get all posts
-app.get('/users', function(req, res) {
-  // Get posts from the mock api
-  // This should ideally be replaced with a service that connects to MongoDB
-  axios.get(API + "/users")
-    .then(function(posts) {
-      res.status(200).json(posts.data);
-    })
-    .catch(function(error) {
-      res.status(500).send(error)
-    });
+app.get("/api/contacts", function(req, res) {
+  db.collection(USER_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 });
 
+/*app.post("/api/contacts", function(req, res) {
+  var newContact = req.body;
+  newContact.createDate = new Date();
 
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '4000';
-app.set('port', port);
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  }
 
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
+  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new contact.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});*/
 
-/**
- * Listen on provided port, on all network interfaces.
+/*  "/api/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
  */
-server.listen(port, function() {console.log('API running on localhost:')});
+
+// app.get("/api/contacts/:id", function(req, res) {
+//   db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+//     if (err) {
+//       handleError(res, err.message, "Failed to get contact");
+//     } else {
+//       res.status(200).json(doc);
+//     }
+//   });
+// });
+//
+// app.put("/api/contacts/:id", function(req, res) {
+//   var updateDoc = req.body;
+//   delete updateDoc._id;
+//
+//   db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+//     if (err) {
+//       handleError(res, err.message, "Failed to update contact");
+//     } else {
+//       updateDoc._id = req.params.id;
+//       res.status(200).json(updateDoc);
+//     }
+//   });
+// });
+//
+// app.delete("/api/contacts/:id", function(req, res) {
+//   db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+//     if (err) {
+//       handleError(res, err.message, "Failed to delete contact");
+//     } else {
+//       res.status(200).json(req.params.id);
+//     }
+//   });
+// });
