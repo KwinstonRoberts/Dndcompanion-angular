883c729f101a9735bf8f9e8130614ccb83f8cf55
KyleW <kyle.w.r@hotmail.com>
2017-02-16 20:45:07 -0500

    still fixing

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -35,8 +35,8 @@ export class InfoComponent implements OnInit {
     // Retrieve posts from the API
     this.usersService.getAllUsers().then((basics: Info[]) => {
       this.basic = basics.map((basic) => {
-        console.log(this.basic)
-        return this.basic[0];
+        console.log(this.basic);
+        return basic;
       });
     });
   }
