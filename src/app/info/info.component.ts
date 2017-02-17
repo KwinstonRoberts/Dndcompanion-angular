02c4bbed485a8ef627fbf2f3ee3ee609c8eb843c
KyleW <kyle.w.r@hotmail.com>
2017-02-17 02:54:26 -0500

    still fixing

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -23,7 +23,7 @@ export class InfoComponent implements OnInit {
   calculate() {
     var tiers = [0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000, 120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000];
     for (var x = 0; x < tiers.length; x++) {
-      if (this.user[0].info.experience < tiers[x]) {
+      if (this.users[0].experience < tiers[x]) {
         this.level = x;
         return;
       }
