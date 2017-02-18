8e9d0632c24be3179266bf750d72945292866046
KyleW <kyle.w.r@hotmail.com>
2017-02-18 03:42:44 -0500

    connection established, level fixed for init, buttons added

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -9,6 +9,24 @@ export class EquipmentComponent implements OnInit {
 
   constructor() { }
 
+  public equipment:string[] = [];
+  private attackId = 1;
+
+
+  removeEquipment(item:string){
+    for(var x=0; x<this.equipment.length; x++){
+      if (this.equipment[x] == item){
+        this.equipment.splice(x,1);
+      }
+    }
+  }
+
+  addEquipment(){
+    this.equipment.push("Equipment #" + this.attackId);
+    this.attackId++;
+  }
+
+
   ngOnInit() {
   }
 
