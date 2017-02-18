507aa4c2542b292ec2f208e0bc0356214aeda22c
KyleW <kyle.w.r@hotmail.com>
2017-02-17 21:47:16 -0500

    connection established, level fixed for init, buttons added

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -34,7 +34,7 @@ export class InfoComponent implements OnInit {
     var tiers = [0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000, 120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000];
     for (var x = 0; x < tiers.length; x++) {
       if (exp < tiers[x]) {
-        this.level = 0;
+        this.level = x;
         return x;
       }
     }
