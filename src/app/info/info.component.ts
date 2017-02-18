c0518a55ec8d74930af7f206c8244fc0e449606a
KyleW <kyle.w.r@hotmail.com>
2017-02-18 08:23:37 -0500

    connection established, level fixed for init, buttons added

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -30,15 +30,16 @@ export class InfoComponent implements OnInit {
     }
   }
 
-  calculate(exp:number) {
+  calculate() {
     var tiers = [0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000, 120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000];
     for (var x = 0; x < tiers.length; x++) {
-      if (exp < tiers[x]) {
+      if (this.selectedUser.info.experience < tiers[x]) {
         this.level = x;
         this.modifierService.setLvlMod(x);
         console.log(this.level);
         return x;
       }
+      this.level = 20;
     }
   }
 
