d685dafe6a236cc49f6bd92ac200be3936f14592
KyleW <kyle.w.r@hotmail.com>
2017-02-20 21:56:24 -0500

    prioritizing manual fields

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -8,6 +8,12 @@ import {ModifierService} from "../modifier.service";
 })
 export class StatsComponent {
 
+  public diceRoll:number = Math.ceil(Math.random()*20);
+
+  rollDice(){
+    this.diceRoll = Math.ceil(Math.random()*20);
+  }
+
   constructor(public modifier:ModifierService){}
   public stats:any[] = [["Strength",0],["Dexterity",0],["Constitution",0],["Intelligence",0], ["Wisdom",0],["Charisma",0]];
 }
